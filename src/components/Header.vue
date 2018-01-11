214b4239ab004418ac1b37dcce72129151034101
adeleparison <adele.parison@hotmail.fr>
2018-01-11 19:43:14 +0100

    Header.vue : rajout paragraphe

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <nav>
-        <h2>{{ title }}</h2>
+        <h1>{{ title }}</h1>
+        <p>{{ para }}</p>
     </nav>
 </template>
 
@@ -10,6 +11,7 @@ export default {
     data() {
         return {
             title : 'DES CARTES JOLIES', 
+            para: 'Envoyez une carte faite par vos soins à vos proches, pour un événement exceptionnel !',
         }
     }
 }
@@ -21,11 +23,21 @@ export default {
         background: #89b6d5;
         margin: 0;
     }
-    h2 {
+    h1 {
         color: #fff;
         margin: 0;
-        padding: 20px;
+        padding-top: 15px;
         text-align: center;
         letter-spacing: 0.05em;
+    }
+    
+    p {
+        color: #fff;
+        text-align: center;
+        letter-spacing: 0.03em;
+        margin: 0;
+        padding-top: 10px;
+        padding-bottom: 15px;
+        
     }
 </style>
