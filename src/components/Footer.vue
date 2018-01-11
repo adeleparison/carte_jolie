2888d63a59f3a75f240a8af62a76850ac4141ea0
adeleparison <adele.parison@hotmail.fr>
2018-01-11 12:27:05 +0100

    Footer.vue : création

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer>
+        <p class="copyright">
+			Copyright © <a href="">A.Parison</a> 2018 – Tous droits réservés. Toute reproduction est interdite sans l'autorisation de l'auteur.
+		</p>
+		<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    </footer>
+</template>
+
+<style>
+</style>
